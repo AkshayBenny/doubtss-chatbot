176b667f7b3e1a5a7510f9f592fe9675c2cf1b00
Akshay Benny <akshay@mx-zenbook>
2023-07-31 22:01:12 +0530

    tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+import TagManager from 'react-gtm-module'
+import { useEffect } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +16,11 @@ export default function RootLayout({
 }: {
 	children: React.ReactNode
 }) {
+	useEffect(() => {
+		// Initialize Google Tag Manager with your GTM ID.
+		TagManager.initialize({ gtmId: 'GTM-54BQ247G' })
+	}, [])
+
 	return (
 		<ClerkProvider>
 			<html lang='en'>
