ae6323965556fb709b28cb7a877ddf3b13da71f8
Akshay Benny <akshay@mx-zenbook>
2023-08-14 21:45:03 +0530

    edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
 import 'server-only'
-
 import ChatPage from '@/components/ChatPage'
-import Script from 'next/script'
-import Head from 'next/head'
 
 export default async function MainChatPage() {
 	return (
 		<>
-		
-			
 			<ChatPage />
 		</>
 	)
