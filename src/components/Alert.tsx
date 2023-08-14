e8ed38bd0d24c01ad5953692e3ca9b53495d59eb
Akshay Benny <akshay@mx-zenbook>
2023-08-14 22:29:54 +0530

    notification icon

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -3,7 +3,7 @@ import { Menu, Transition } from '@headlessui/react'
 import { Fragment, useEffect, useRef, useState } from 'react'
 import { Bars3Icon } from '@heroicons/react/20/solid'
 import Image from 'next/image'
-import NotificationIcon from 'remixicon-react/NotificationLineIcon'
+import Notification3LineIcon from 'remixicon-react/Notification3LineIcon'
 import DeleteBinLineIcon from 'remixicon-react/DeleteBinLineIcon'
 import { chatHistory, userData } from '@/state/recoil'
 import { useRecoilState } from 'recoil'
@@ -54,7 +54,7 @@ export default function Alert() {
 				className='relative inline-block text-left'>
 				<div>
 					<Menu.Button className='p-3 border border-custom-gray rounded-xl flex items-center justify-center'>
-						<NotificationIcon
+						<Notification3LineIcon
 							className='h-[18px] w-[18px] text-violet-200 hover:text-violet-100'
 							aria-hidden='true'
 						/>
