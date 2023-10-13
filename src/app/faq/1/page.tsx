7ba72e3656f0da8dbdf07de45fe5a7ebe48055c5
Akshay Benny <akshay@mx-zenbook>
2023-10-13 15:20:00 +0530

    faq page

---
+++ src/app/faq/1/page.tsx
@@ -0,0 +1,112 @@
+import 'server-only'
+import Footer from '@/components/Footer'
+import ArrowRightSLineIcon from 'remixicon-react/ArrowRightSLineIcon'
+import Link from 'next/link'
+
+export default function HelpAndFAQ() {
+	return (
+		<div className='text-custom-white overflow-x-none w-full min-h-screen'>
+			<div className='max-w-6xl mx-auto'>
+				<div className='w-full h-full max-w-6xl mx-auto'>
+					<h2 className='font-extrabold text-[44px] '>
+						What is Doubtss.com?
+					</h2>
+					<div className='flex items-center justify-start gap-4'>
+						<div className='w-[40px] h-[40px] rounded-lg bg-custom-green'></div>
+						<div className='space-y-[2px]'>
+							<p className='font-medium text-base'>John Doe</p>
+							<p className='opacity-60 text-sm tracking-[-1%]'>
+								Updated over a week ago
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className='mt-[80px]'>
+					<h4 className='font-semibold text-2xl pb-[8px]'>
+						How much does it cost to use Doutbss.com?
+					</h4>
+					<ul className='list-inside list-disc font-normal text-base'>
+						<li>The research preview of ChatGPT is free to use.</li>
+					</ul>
+					<h4 className='pt-[32px] font-semibold text-2xl pb-[8px]'>
+						How does Doutbss.com work?
+					</h4>
+					<ul className='list-inside list-disc font-normal text-base space-y-[8px]'>
+						<li>
+							Doubtss.com is fine-tuned from GPT-3.5, a language
+							model trained to produce text. ChatGPT was optimized
+							for dialogue by using Reinforcement Learning with
+							Human Feedback (RLHF) – a method that uses human
+							demonstrations and preference comparisons to guide
+							the model toward desired behavior. ChatGPT is
+							fine-tuned from GPT-3.5, a language model trained to
+							produce text. ChatGPT was optimized for dialogue by
+							using Reinforcement Learning with Human Feedback
+							(RLHF) – a method that uses human demonstrations and
+							preference comparisons to guide the model toward
+							desired behavior.
+						</li>
+						<li>
+							Doubtss.com is fine-tuned from GPT-3.5, a language
+							model trained to produce text. ChatGPT was optimized
+							for dialogue by using Reinforcement Learning with
+							Human Feedback (RLHF) – a method that uses human
+							demonstrations and preference comparisons to guide
+							the model toward desired behavior. ChatGPT is
+							fine-tuned from GPT-3.5, a language model trained to
+							produce text. ChatGPT was optimized for dialogue by
+							using Reinforcement Learning with Human Feedback
+							(RLHF) – a method that uses human demonstrations and
+							preference comparisons to guide the model toward
+							desired behavior.
+						</li>
+					</ul>
+					<h4 className='pt-[32px] font-semibold text-2xl pb-[8px]'>
+						How does Doutbss.com work?
+					</h4>
+					<ul className='list-inside list-disc font-normal text-base space-y-[8px]'>
+						<li>
+							Doubtss.com is fine-tuned from GPT-3.5, a language
+							model trained to produce text. ChatGPT was optimized
+							for dialogue by using Reinforcement Learning with
+							Human Feedback (RLHF) – a method that uses human
+							demonstrations and preference comparisons to guide
+							the model toward desired behavior.
+						</li>
+					</ul>
+
+					<h3 className='font-extrabold text-[44px] pt-[160px]'>
+						Related articles
+					</h3>
+					<div className='pt-12 space-y-[24px] mb-[160px]'>
+						<Link
+							href='/faq/1'
+							className='flex items-center justify-between px-6 py-7 border rounded-2xl border-white border-opacity-20'>
+							<p className='font-medium text-xl'>
+								How does Doubtss.com work?
+							</p>
+							<ArrowRightSLineIcon />
+						</Link>
+						<Link
+							href='/faq/1'
+							className='flex items-center justify-between px-6 py-7 border rounded-2xl border-white border-opacity-20'>
+							<p className='font-medium text-xl'>
+								What is the cost of using Doubtss.com?
+							</p>
+							<ArrowRightSLineIcon />
+						</Link>
+						<Link
+							href='/faq/1'
+							className='flex items-center justify-between px-6 py-7 border rounded-2xl border-white border-opacity-20'>
+							<p className='font-medium text-xl'>
+								Is there a trial period for Doubtss.com?
+							</p>
+							<ArrowRightSLineIcon />
+						</Link>
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</div>
+	)
+}
